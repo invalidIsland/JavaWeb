6c1758d41aa686b0efe09bed4c8fccbf957fd5c3
maomao <1449573893@qq.com>
2021-12-13 18:18:46 +0800

    第一次提交

---
+++ src/main/java/com/grm/SpringbootJavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.grm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJavaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grm/SpringbootJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJavaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
